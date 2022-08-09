8a4834586d136f63356050afa1eb625a21e75539
OlegYhay <108109792+OlegYhay@users.noreply.github.com>
2022-08-09 13:22:12 +0500

    all

--- config/config/settings.py
+++ config/config/settings.py
@@ -166,5 +166,7 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-STATIC_ROOT = ''
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
