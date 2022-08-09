3042cc18c12c4350a4cdd11a376d105a588de20d
OlegYhay <108109792+OlegYhay@users.noreply.github.com>
2022-08-09 10:12:10 +0500

    all

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh -c 'cd config && gunicorn config.wsgi --log-file=-'
--- config/config/settings.py
+++ config/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -159,3 +159,8 @@ DEFAULT_FROM_EMAIL = 'jadk.fre@mail.ru'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
