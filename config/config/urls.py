8b561cbecc40306f2afbf183f637d64e02fd9c2d
OlegYhay <108109792+OlegYhay@users.noreply.github.com>
2022-08-09 13:48:03 +0500

    all

--- config/config/urls.py
+++ config/config/urls.py
@@ -29,3 +29,4 @@ urlpatterns = [
                   path('books/', include('books.urls')),
                   path('news/', include('news.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
